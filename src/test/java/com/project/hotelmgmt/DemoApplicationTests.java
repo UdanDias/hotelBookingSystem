632a4bf64a7e53977c112515c264ff439e941a88
UdanDias <udandias892@gmail.com>
2026-09-17 20:01:24 +0530

    Initial commit

---
+++ src/test/java/com/project/hotelmgmt/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hotelmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
